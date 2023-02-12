89e46068e03f6e7b2f1db79bcbd6983b5c67e13b
Jessica Joseph <jessicajoseph2019@yahoo.com>
2023-02-12 11:19:22 +0100

    feat:Header user interface completed :)

--- src/elements/Header.tsx
+++ src/elements/Header.tsx
@@ -9,7 +9,7 @@ function Header() {
     <h1 className="text-2xl font-bold w-96 lg:pt-10 pt-0">Awesome Design Best Furniture For Your Home</h1>
       <p className="text-sm font-light py-4">Browse, Choose & Order, Very Simple</p>
       <Button text="Shop Now"/>
-      <section className="flex justify-between pt-28">
+      <section className="flex justify-between lg:pt-28 pt-16">
         <div className="flex flex-col w-28 pb-4">
             <h1 className="text-2xl font-semibold">1000+</h1>
             <p className="text-sm font-light">Furnitures & Equipments</p>
@@ -25,9 +25,8 @@ function Header() {
       </section>
     </figcaption>
     <figure>
-        <img src ={header} alt="living room furniture" className="lg:w-9/12 lg:h-9/12 w-full"/>
+        <img src ={header} alt="living room furniture" className="lg:w-9/12 lg:h-9/12 w-full lg:pt-0 pt-4"/>
     </figure>
-     
     </header>
   );
 }
