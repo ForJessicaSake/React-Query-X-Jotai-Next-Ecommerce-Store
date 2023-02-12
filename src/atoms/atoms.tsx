f81d7033a1fe57e8fd4e15538e92115c1ddf5bb0
Jessica Joseph <jessicajoseph2019@yahoo.com>
2023-02-12 02:22:55 +0100

    feat:completed the user interface for collections

--- src/atoms/atoms.tsx
+++ src/atoms/atoms.tsx
@@ -1,7 +1,7 @@
 import {atom} from "jotai"
 
 const navigation = atom<boolean>(false)
-const collection = atom<boolean>(false)
+const femalecollection = atom<boolean>(false)
 const cart = atom<number>(0)
 
-export {navigation, collection, cart} 
+export {navigation, femalecollection, cart} 
